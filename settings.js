6b668830f533042d952d47ca9ff2171a15d6bc1e
ADxVAL <154498693+ADxVAL@users.noreply.github.com>
2024-08-02 21:01:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
+global.botname = 'jnm' //ur bot name
+global.ownernumber = '2347061141859' //ur owner number
 global.ownername = '🦄드림 가이 Xeon' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
@@ -29,9 +29,9 @@ global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.creator = "2347061141859@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["2347061141859"] // Premium User
 global.hituet = 0
 
 //bot sett
